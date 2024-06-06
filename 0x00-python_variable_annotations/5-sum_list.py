33a93a9db65673e8a7590ab04bec5b278e3809be
FahdAzouz <fahdstudies00@gmail.com>
2024-06-06 13:19:46 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 5'module '''
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    '''return the sum of floats'''
+    sum = 0
+    for num in input_list:
+        sum += num
+    return sum
