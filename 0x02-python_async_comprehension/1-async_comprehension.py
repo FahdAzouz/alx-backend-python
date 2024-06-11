2e267d2a55d19a012530f34d27167e344d7887fa
FahdAzouz <fahdstudies00@gmail.com>
2024-06-11 18:44:52 +0100

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 '''Task 1 module'''
 import asyncio
-
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     '''collect 10 numbers and return them'''
     result = [i async for i in async_generator()]
     return result
